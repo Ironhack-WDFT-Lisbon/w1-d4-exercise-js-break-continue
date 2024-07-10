9b13c5b1a863e737ad1a542ccc45e596a6a14688
ahpoise <36297960+ahpoise@users.noreply.github.com>
2024-07-10 13:15:34 +0100

    update exercise jul 24

--- script.js
+++ script.js
@@ -5,35 +5,7 @@ let i = 0;
 // ------------------------------
 // Iteration 1
 // ------------------------------
-// while (i <= 20) {
-//   switch (i % 2) {
-//     case 0:
-//       console.log(`${i} is even`);
-//       break;
-//     default:
-//       console.log(`${i} is odd`);
-//       break;
-//   }
-//   i++;
-// }
 
 // ------------------------------
-// Iteration 1
+// Iteration 2
 // ------------------------------
-// while (i <= 20) {
-//   if (i !== 0 && i % 7 === 0) {
-//     console.log(`${i} is divisible by 7`);
-//     i++;
-//     continue;
-//   }
-
-//   switch (i % 2) {
-//     case 0:
-//       console.log(`${i} is even`);
-//       break;
-//     default:
-//       console.log(`${i} is odd`);
-//       break;
-//   }
-//   i++;
-// }
